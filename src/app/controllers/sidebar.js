2bed72e431164f3c59fbb57f1b5c075aff9ca6ae
John Briggs <john.briggs@imagineeasy.com>
2014-09-07 23:20:32 -0400

    added active state for home page

--- src/app/controllers/sidebar.js
+++ src/app/controllers/sidebar.js
@@ -10,12 +10,13 @@ define([
    * @classdesc Controller for the Sidebar view
    * @ngInject
    */
-  var SidebarCtrl = function ($rootScope, comicsFactory) {
+  var SidebarCtrl = function ($rootScope, comicsFactory, $state, $scope) {
     var _this = this;
-
+    $scope.$state = $state;
     comicsFactory.getComics()
       .then(function(result) {
         $rootScope.data.comics = result.data;
+
       }, function(error) {
         console.log(error);
       });
