5228bfae6d6667916869e3f4fcc4026fe3814052
John Briggs <john.briggs@imagineeasy.com>
2014-09-12 22:34:36 -0400

    added markdown support. yay.

---
+++ src/app/directives/markdown.js
@@ -0,0 +1,20 @@
+define([
+  'app/app',
+  'showdown'
+], function(app) {
+  'use strict';
+
+  var markdown = function ($http) {
+    var converter = new Showdown.converter();
+    return {
+      restrict: 'A',
+      scope: { link:'@' },
+      link: function (scope, element, attrs) {
+        var htmlText = converter.makeHtml(element.html());
+        element.html(htmlText);
+      }
+    };
+  };
+
+  return app.directive('markdown', markdown);
+});
